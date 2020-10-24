177a05fb21351d5865a89075492532e6e280c69d
HaiNguyen3112 <nguyenhai311294@gmail.com>
2020-10-24 12:02:28 +0700

    hai_create_model

---
+++ src/app/model/bookingTicketDTO.ts
@@ -0,0 +1,11 @@
+export class BookingTicketDTO {
+    id: number;
+    code: string;
+    booking_date: Date;
+    customerCode: string;
+    customerName: string;
+    customerCardId: string;
+    customerPhone: string;
+    movieName: string
+    status: boolean;
+}
