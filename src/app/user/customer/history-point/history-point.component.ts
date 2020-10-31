5cb962f845c30b71934f43be7b26c92aed3441ca
thinhhv1993 <64394302+thinhhv1993@users.noreply.github.com>
2020-10-31 16:22:44 +0700

    a

--- src/app/user/customer/history-point/history-point.component.ts
+++ src/app/user/customer/history-point/history-point.component.ts
@@ -4,6 +4,10 @@ import {Point} from '../model/Point';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {Subscription} from 'rxjs';
 import {ActivatedRoute, ParamMap} from '@angular/router';
+import {Account} from '../model/Account';
+import {Customer} from '../model/Customer';
+import {CustomerService} from '../service/customer.service';
+import {AccountService} from '../service/account.service';
 
 @Component({
     selector: 'app-history-point',
@@ -23,8 +27,18 @@ export class HistoryPointComponent implements OnInit {
     totalPage: number;
     jumpPage: any;
     sub: Subscription;
+    account: Account;
+    customer: Customer;
+    rescus: string;
+    resacc: string;
+    idAc: number;
 
-    constructor(private pointService: PointService, private fb: FormBuilder, private activatedRoute: ActivatedRoute) {
+    constructor(private fb: FormBuilder
+        , private activatedRoute: ActivatedRoute
+        , private customerService: CustomerService
+        , private accountService: AccountService
+        , private pointService: PointService
+    ) {
     }
 
     ngOnInit(): void {
@@ -33,7 +47,51 @@ export class HistoryPointComponent implements OnInit {
             contractEndDate: ['', [Validators.required, Validators.pattern('^\\d{4}\\-(0?[1-9]|1[012])\\-(0?[1-9]|[12][0-9]|3[01])$')]],
             gender: new FormControl('', Validators.required)
         });
-        this.searchPoint();
+
+
+        this.sub = this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
+            const key = paramMap.get('id');
+            this.myFunction(key);
+            this.customerService.getCustomerById(this.rescus).subscribe(
+                next => {
+                    this.customer = next[0];
+                    this.myFunction1(this.customer.accountId);
+                    this.accountService.getAccountById(this.resacc).subscribe(
+                        next => {
+                            this.account = next[0];
+                            this.idAc = this.account.id;
+                        },
+                        error => {
+                            console.log(error);
+                            this.account = null;
+                        }
+                    );
+                },
+                error => {
+                    console.log(error);
+                    this.customer = null;
+                }
+            );
+        });
+
+        this.sub = this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
+            const key = paramMap.get('id');
+            this.pointService.getTickerPlusById(this.idAc, this.currentPage, '2020-01-01', '2021-01-01').subscribe(next => (this.pointS = next), error => (this.pointS = []));
+            this.pointService
+                .getTickerListPlusById(this.idAc, this.star, this.end)
+                .subscribe(next => {
+                    (this.pointListS = next
+                        , this.totalEntities = this.pointListS.length
+                        , this.totalPage = this.totalEntities / 10);
+                    if (this.pointListS.length === 0) {
+                        this.message = 'not Data!';
+                    } else {
+                        this.message = '';
+                    };
+                    error => (this.pointListS = []);
+
+                });
+        });
     }
 
 
@@ -67,24 +125,23 @@ export class HistoryPointComponent implements OnInit {
     // tra cứu điểm
     searchPoint() {
         if (this.Plus == 'true') {
-            this.sub = this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
-                const key = paramMap.get('id');
-                this.pointService.getTickerPlusById(key, this.currentPage, this.star, this.end).subscribe(next => (this.pointS = next), error => (this.pointS = []));
-                this.pointService
-                    .getTickerListPlusById(key, this.star, this.end)
-                    .subscribe(next => {
-                        (this.pointListS = next
-                            , this.totalEntities = this.pointListS.length
-                            , this.totalPage = this.totalEntities / 10);
-                        if (this.pointListS.length === 0) {
-                            this.message = 'not Data!';
-                        } else {
-                            this.message = '';
-                        };
-                            error => (this.pointListS = []);
+            this.pointService.getTickerPlusById(this.idAc, this.currentPage, this.star, this.end).subscribe(next => (this.pointS = next), error => (this.pointS = []));
+            this.pointService
+                .getTickerListPlusById(this.idAc, this.star, this.end)
+                .subscribe(next => {
+                    (this.pointListS = next
+                        , console.log(this.pointListS)
+                        , this.totalEntities = this.pointListS.length
+                        , this.totalPage = this.totalEntities / 10);
+                    if (this.pointListS.length === 0) {
+                        this.message = 'not Data!';
+                    } else {
+                        this.message = '';
+                    }
+                    ;
+                    error => (this.pointListS = []);
+                });
 
-                    });
-            });
         }
         if (this.Plus == 'false') {
             this.sub = this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
@@ -102,9 +159,22 @@ export class HistoryPointComponent implements OnInit {
                         } else {
                             this.message = '';
                         }
+                        ;
+                        error => (this.pointListS = []);
                     });
             });
         }
+    }
+
+
+    myFunction(id: string) {
+        let uri = 'filter={"property":"id","operator":"eq","value":' + id + '}';
+        this.rescus = encodeURI(uri);
+    }
+
+    myFunction1(id: number) {
+        let uri = 'filter={"property":"id","operator":"eq","value":' + id + '}';
+        this.resacc = encodeURI(uri);
     }
 
     changeGender(e) {
