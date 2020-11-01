9bae1dcecf8b6debdcb49053933e341f4e92be68
tuongvule <tuongvule@gmail.com>
2020-11-01 17:50:49 +0700

    dd

--- src/app/admin/news/news.service.ts
+++ src/app/admin/news/news.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Movie} from '../../model/Movie';
 import {News} from '../../model/News';
+import {Hall} from '../../model/Hall';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,7 @@ import {News} from '../../model/News';
 export class NewsService {
   private readonly API_URL_NEWS = 'http://localhost:8080/other';
   private readonly API_URL_NEWS_2 = 'http://localhost:8080/news2';
+  private readonly API_THREE_LAST_NEWS = 'http://localhost:8080/getThreeLastNews';
 
   constructor(private http: HttpClient) { }
 
@@ -35,5 +37,10 @@ export class NewsService {
   //delete News
   deleteNews(id: number): Observable<News>{
     return this.http.delete<News>(this.API_URL_NEWS + '/' + id);
+  }
+
+  //get 3 last news to show on home screen
+  getThreeLastNews(): Observable<News[]>{
+    return this.http.get<News[]>(this.API_THREE_LAST_NEWS);
   }
 }
