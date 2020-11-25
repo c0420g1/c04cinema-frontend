075107fac76af762246bd101ac5187591f346c69
quocnna <63178225+quocnna@users.noreply.github.com>
2020-11-25 14:58:16 +0700

    quoc parking

--- src/app/user/newsdetail/newsdetail.component.ts
+++ src/app/user/newsdetail/newsdetail.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./newsdetail.component.css']
 })
 export class NewsdetailComponent implements OnInit {
-
+  items: number[]= [1,2,3,4,5,6,7,8,8,9,2,2,22,3,3,3];
   constructor() { }
 
   ngOnInit(): void {
